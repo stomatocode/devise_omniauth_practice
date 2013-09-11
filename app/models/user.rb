e67298cb18101d079340cec7d94753abccca5e7f
Dan Hossom <dan@Dans-MacBook-Pro-2.local>
2013-09-11 12:17:01 -0700

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
+  before_filter :authenticate_user!
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DevisePractice::Application.config.secret_key_base = '5981c55f0dd07de87dac143b7cf2c05ff31f0ed56fa3292d4139eb0808447e9d412ff9686acf76da2a57801106ebfd818bd771eebfabaa1f180fbbcf565b4b0c'
